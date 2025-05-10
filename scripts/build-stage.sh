f3398cbfd3e1abef2c90abba7219744cde9037f8
Oleg <84809257+swugerd@users.noreply.github.com>
2025-05-10 17:11:40 +0300

    ci: change ci config (#4)

--- scripts/build-stage.sh
+++ scripts/build-stage.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 echo "🔧 Building staging image..."
 
-cd ~/projects/quakke_backend/scripts
+cd ~/projects/quakke-backend
+
+if [ -n "$BRANCH" ]; then
+  echo "📥 Pulling branch: $BRANCH"
+  git fetch origin "$BRANCH"
+  git checkout "$BRANCH" || git checkout -b "$BRANCH" origin/"$BRANCH"
+  git reset --hard origin/"$BRANCH"
+else
+  echo "⚠️ BRANCH variable is not set. Skipping git pull."
+fi
+
+cd scripts
 docker build -t quakke-backend:stage .
